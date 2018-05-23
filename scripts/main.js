50826c8a60e4969b869bad611ff9921590048ef0
Deanna Vickers <deanna2000@gmail.com>
2018-05-23 09:37:09 -0500

    finished exercise

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// Function to add event listener to header 1
+var h1el = document.getElementById("header1");
+h1el.addEventListener("click", function(){
+    console.log(h1el)
+})
+
+// Function to add event listener to header 2
+var h2el = document.getElementById("header2");
+h2el.addEventListener("click", function(){
+    console.log(h2el)
+})
+
+// Function to add event listener to header 3
+var h3el = document.getElementById("header3");
+h3el.addEventListener("click", function(){
+    console.log(h3el)
+})
+
